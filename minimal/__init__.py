05c5cc7a2222a1f268bafd820717e6eb61b9f8df
LuisHRiosC18 <141874238+LuisHRiosC18@users.noreply.github.com>
2023-11-22 14:41:50 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,8 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+
+def everybody():
+    for i in range(0,9):
+        print('EVERYBODYYYYYYY')
+    return 
